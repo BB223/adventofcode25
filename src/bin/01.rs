17a439bc1a10581e154f7f791f61820ef81569c8
BB223 <79644542+BB223@users.noreply.github.com>
2025-12-01 09:44:59 +0100

    day 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,9 +1,59 @@
-pub fn part_one(input: &str) -> Option<u64> {
-    None
+pub fn part_one(input: &str) -> Option<u32> {
+    let lines = input.lines();
+
+    let mut dial = 50;
+    let mut solution = 0;
+
+    for line in lines {
+        let chars = line.split_at(1);
+        let char = chars.0;
+        let times: i32 = chars.1.parse().unwrap();
+        dial = if char == "R" {
+            (dial + times).rem_euclid(100)
+        } else {
+            (dial - times).rem_euclid(100)
+        };
+        if dial == 0 {
+            solution += 1;
+        }
+    }
+
+    Some(solution)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<i32> {
+    let lines = input.lines();
+
+    let mut dial = 50;
+    let mut solution = 0;
+
+    for line in lines {
+        let chars = line.split_at(1);
+        let char = chars.0;
+        let times: i32 = chars.1.parse().unwrap();
+        dial = if char == "R" {
+            let passes = times / 100;
+            let rotation = dial + (times - passes * 100);
+            solution += passes;
+            if rotation > 100 {
+                solution += 1;
+            }
+            rotation.rem_euclid(100)
+        } else {
+            let passes = times / 100;
+            let rotation = dial - (times - passes * 100);
+            solution += passes;
+            if rotation < 0 && dial != 0 {
+                solution += 1;
+            }
+            rotation.rem_euclid(100)
+        };
+        if dial == 0 {
+            solution += 1;
+        }
+    }
+
+    Some(solution)
 }
 
-advent_of_code_25::advent_of_code!(1, None, None);
+advent_of_code_25::advent_of_code!(1, Some(3), Some(6));
