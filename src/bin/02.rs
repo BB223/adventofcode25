1ac03e243c6136bddde07ec3f863eff55e615745
BB223 <79644542+BB223@users.noreply.github.com>
2025-12-02 10:44:29 +0000

    day 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,9 +1,43 @@
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    let ranges = input.split(',');
+    let solution = ranges
+        .into_iter()
+        .flat_map(|range| {
+            let numbers = range.split_once("-").unwrap();
+            let lower: u64 = numbers.0.parse().unwrap();
+            let upper: u64 = numbers.1.parse().unwrap();
+
+            lower..upper + 1
+        })
+        .filter(|number| (number.checked_ilog10().unwrap_or(0) + 1).is_multiple_of(2))
+        .filter(|number| {
+            let num_str = number.to_string();
+            let numbers = num_str.split_at(num_str.len() / 2);
+            numbers.0 == numbers.1
+        })
+        .sum();
+
+    Some(solution)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let ranges = input.split(',');
+    let solution = ranges
+        .into_iter()
+        .flat_map(|range| {
+            let numbers = range.split_once("-").unwrap();
+            let lower: u64 = numbers.0.parse().unwrap();
+            let upper: u64 = numbers.1.parse().unwrap();
+
+            lower..upper + 1
+        })
+        .filter(|number| {
+            let doubled = format!("{number}{number}");
+            doubled[1..doubled.len() - 1].contains(&number.to_string())
+        })
+        .sum();
+
+    Some(solution)
 }
 
-advent_of_code_25::advent_of_code!(2, None, None);
+advent_of_code_25::advent_of_code!(2, Some(1227775554), Some(4174379265));
